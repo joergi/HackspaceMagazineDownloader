7ff131671d0fca24f172aed5e5037f601c45a7c0
Joergi <joergi@users.noreply.github.com>
2017-12-11 23:47:08 +0100

    bugfix

--- windows-downloader.ps1
+++ windows-downloader.ps1
@@ -11,19 +11,19 @@
 # ------------------------------------------------------------------
 
 # Check if directory dont exist and try create
-if( -Not (Test-Path -Path "special_issues" ) )
+if( -Not (Test-Path -Path "issues" ) )
 {
-    New-Item -ItemType directory -Path "special_issues"
+    New-Item -ItemType directory -Path "issues"
 }
 
-$special_issues = @(
+$issues = @(
     "HackSpaceMag01.pdf
 )
 
 $baseUrl="https://s3-eu-west-1.amazonaws.com/rpi-magazines/issues/full_pdfs/000/000/007/original/"
 $web = New-Object system.net.webclient
 $errorCount = 0
-foreach($issue in $special_issues)
+foreach($issue in $issues)
 {
       try
                 {
