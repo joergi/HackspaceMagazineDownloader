e6228da0e0528d55f874244535df7685e656fae8
Joergi <joergi@users.noreply.github.com>
2017-12-12 01:12:22 +0100

    linux bugfix

--- linux-downloader.sh
+++ linux-downloader.sh
@@ -18,44 +18,28 @@ fi
 i=1
 issues=1
 
-if [ -z $1 ]
-then
-	wget https://s3-eu-west-1.amazonaws.com/rpi-magazines/issues/full_pdfs/000/000/007/original/ \
-	--recursive \
-	--no-directories \
-	--no-parent \
-	--directory-prefix issues \
-	--mirror \
-	--accept-regex "/HackSpaceMag[0-9]+\.pdf" \
-	--reject index.html \
-	--execute robots=off
+while :
+do
+	case "$1" in
+	-f) shift; i="$1";;
+	-l) shift; issues="$1";;
+	--) shift; break;;
+	-*) usage "bad argument $1";;
+	*) break;;
+	esac
+	shift
+done
 
-else
+while [ $i -le $issues ]
+do
+	if [ "$i" -lt 10 ]; then
+		wget https://s3-eu-west-1.amazonaws.com/rpi-magazines/issues/full_pdfs/000/000/007/original/HackSpaceMag0$i.pdf -P issues/
 
-	while :
-	do
-		case "$1" in
-		-f) shift; i="$1";;
-		-l) shift; issues="$1";;
-		--) shift; break;;
-		-*) usage "bad argument $1";;
-		*) break;;
-		esac
-		shift
-	done
-
-	while [ $i -le $issues ]
-	do
-		if [ "$i" -lt 10 ]; then
-			wget https://s3-eu-west-1.amazonaws.com/rpi-magazines/issues/full_pdfs/000/000/007/original/HackSpaceMag0$i.pdf -P issues/
-
-		else
-			wget https://s3-eu-west-1.amazonaws.com/rpi-magazines/issues/full_pdfs/000/000/007/original/HackSpaceMag$i.pdf -P issues/
-		fi
-		i=$(( i+1 ))
-	done
-
-fi
+	else
+		wget https://s3-eu-west-1.amazonaws.com/rpi-magazines/issues/full_pdfs/000/000/007/original/HackSpaceMag$i.pdf -P issues/
+	fi
+	i=$(( i+1 ))
+done
 
 
 
