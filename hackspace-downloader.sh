090040917b7cbb9392fa30e64e1a230adf3a6c36
Joergi <1439809+joergi@users.noreply.github.com>
2020-07-28 01:12:05 +0200

    Update hackspace-downloader.sh

--- hackspace-downloader.sh
+++ hackspace-downloader.sh
@@ -22,7 +22,7 @@ file="issues.txt";
 recentIssue=$(cat "$file");
 
 # shellcheck disable=SC1090
-source <(curl -s https://raw.githubusercontent.com/joergi/downloader/master/linux_mac/downloader.sh) "$downloadUrl" "$recentIssue" "$@"
+source <(curl -s https://raw.githubusercontent.com/joergi/downloader/main/linux_mac/downloader.sh) "$downloadUrl" "$recentIssue" "$@"
 
 exit 0
 
