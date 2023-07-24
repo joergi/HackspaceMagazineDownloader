b16060ed14cb58bae9fa9825fbe05e5c339e9de8
joergi <1439809+joergi@users.noreply.github.com>
2023-07-24 23:37:36 +0200

    adds script for using-latest version
    based on the original script from @keni-n for the magpi downloader
    
    #29

---
+++ script-for-using-latest-version.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -o errexit
+#set -o pipefail
+set -o nounset
+IFS=$'\n\t'
+
+# ------------------------------------------------------------------
+# based on the script Kenichi Nagai https://github.com/keni-n/
+# https://github.com/joergi/MagPiDownloader/blob/main/linux_mac/magpi-issue-downloader-latest.sh
+# ------------------------------------------------------------------
+
+# VERSION=0.1
+# you don't need to run this script
+# this is running automatically on Github each date to update the latest issues
+
+MAGPI_URL="https://hackspace.raspberrypi.com/"
+BASEDIR=$(dirname "$0")
+
+#printf -v page_url "$MAGPI_URL/issues"
+page_url=$(printf '%s\n' "$MAGPI_URL/issues")
+
+latest_issue=$(curl -sf "$page_url" | grep "Get PDF" | head -n 1 | sed 's/^.*issues\///' | sed 's/\/pdf.*$//')
+echo "Latest Issue is " "$latest_issue"
+
+file="$BASEDIR/issues.txt";
+echo $file
+echo "$latest_issue" >"$file"
