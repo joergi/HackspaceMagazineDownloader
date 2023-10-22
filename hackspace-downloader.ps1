686b2b0f581c7d87590dca044fa9cf95771caa53
joergi <1439809+joergi@users.noreply.github.com>
2023-10-22 22:54:09 +0200

    fixes naming from windows downloads
    closes #61

--- hackspace-downloader.ps1
+++ hackspace-downloader.ps1
@@ -15,6 +15,7 @@ param(
     [string]$FirstIssue = "1",
     [string]$LastIssue
 )
+
 [uri]$baseUrl = "https://hackspace.raspberrypi.org/issues/"
 
 # control variables
@@ -25,21 +26,24 @@ if (!$LastIssue) {
 
 $downloadDir = Join-Path $baseDir "issues"
 
-# Check if directory dont exist and try create
+# Check if directory doesn't exist and try to create it
 if (!(Test-Path -Path $downloadDir)) {
     $null = New-Item -ItemType Directory -Path $downloadDir -ErrorAction Stop
 }
 
 $errorCount = 0
-foreach($issue in $FirstIssue..$LastIssue) {
-    $uri = [uri]::new($baseurl,"{0:00}/pdf/download" -f $issue)
+foreach ($issue in $FirstIssue..$LastIssue) {
+    $uri = [uri]::new($baseUrl, "{0:00}/pdf/download" -f $issue)
     Write-Verbose -Message "Downloading $uri"
     if (($link = (Invoke-WebRequest -UseBasicParsing $uri).Links.Where{ $_.class -eq "c-link" }.href)) {
-        $uri = [uri]::new($baseurl, $link)
+        $uri = [uri]::new($baseUrl, $link)
         try {
+            # Replace this line to specify the output file name
+            $fileName = "HS_{0:00}.pdf" -f $issue
+            $filePath = Join-Path $downloadDir $fileName
             Write-Information "Downloading $uri"
             Write-Output "Downloading $uri"
-            Invoke-WebRequest $uri -OutFile (Join-Path $downloadDir $uri.Segments[-1]) -ErrorAction Stop
+            Invoke-WebRequest $uri -OutFile $filePath -ErrorAction Stop
             Write-Verbose -Message "Downloaded $uri"
         } catch {
             Write-Warning "Failed downloading $uri"
